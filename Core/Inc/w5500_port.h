4f8d57ebfc12a1a31ea05cee632dd1ccb91c8819
kgsinh <ksrgohil@gmail.com>
2026-08-09 01:50:37 -0400

    - Ethernet bring up is done till socket 0 is listening on port:5000 using W5500

---
+++ Core/Inc/w5500_port.h
@@ -0,0 +1,15 @@
+/*
+ * w5500_port.h
+ *
+ *  Created on: Aug 8, 2026
+ *      Author: ksrgo
+ */
+
+#ifndef INC_W5500_PORT_H_
+#define INC_W5500_PORT_H_
+
+#include <stdio.h>
+
+void w5500_port_init(void);
+
+#endif /* INC_W5500_PORT_H_ */
